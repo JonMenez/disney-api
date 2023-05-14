b6388a3eb8bfb1faef1291725b0291ff8908e471
JonMenez <joanf.gimenez@gmail.com>
2023-05-14 09:58:39 -0300

    [ADD] class server to manage it

---
+++ app/models/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const { authRouter, characterRouter, contentRouter, genreControllers, userRouter } = require('../routes');
+const db = require('../../config/database');
+
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+        this.paths = {
+            auth: '/api/v1/auth',
+            characters: '/api/v1/characters',
+            contents: '/api/v1/contents',
+            genres: '/api/v1/genres',
+            users: '/api/v1/users',
+        }
+
+        this.connectDB()
+        this.middlewares()
+        this.routes()
+    }
+
+    async connectDB() {
+        try {
+            await db.sync({ alter: true });
+            // await db.authenticate();
+            console.log('Database connected');
+        } catch (error) {
+            console.log(error);
+            console.log('It could not connect to the database');
+        }
+    }
+
+    middlewares() {
+        //CORS
+        this.app.use(cors())
+        //Static files
+        // this.app.use(express.static('public'));
+        //Body parser
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use(this.paths.auth, authRouter);
+        this.app.use(this.paths.characters, characterRouter);
+        this.app.use(this.paths.contents, contentRouter);
+        this.app.use(this.paths.genres, genreControllers);
+        this.app.use(this.paths.users, userRouter);
+    }
+
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server running on port ${this.port}`);
+        });
+    }
+}
+
+module.exports = Server;

8cef2fa5e5af959c2347cfd430db78f9fc4f0a4a
JonMenez <joanf.gimenez@gmail.com>
2023-05-14 21:14:34 -0300

    [ADD} swagger config

--- app/models/server.js
+++ app/models/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 const { authRouter, characterRouter, contentRouter, genreControllers, userRouter } = require('../routes');
 const db = require('../../config/database');
 
@@ -39,6 +41,8 @@ class Server {
         // this.app.use(express.static('public'));
         //Body parser
         this.app.use(express.json());
+        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
     }
 
     routes() {

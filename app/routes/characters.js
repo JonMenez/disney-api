76b91cdeeee520e9eeba81ec6d0c7987d8277772
JonMenez <joanf.gimenez@gmail.com>
2023-05-14 21:14:48 -0300

    {DEL} file

--- app/routes/characters.js
+++ app/routes/characters.js
@@ -6,10 +6,10 @@ const router = Router()
 router.get('/', characterControllers.getCharacters)
 router.get('/:id', characterControllers.getCharacter)
 router.post('/', characterControllers.createCharacter)
+router.delete('/:id', characterControllers.deleteCharacter)
+router.put('/:id', characterControllers.updateCharacter)
 router.get('/:id/content', characterControllers.getContents)
 router.post('/:id/content', characterControllers.addContent)
-router.post('/:id/content/content_id', characterControllers.addContent)
-router.put('/:id', characterControllers.updateCharacter)
-router.delete('/:id', characterControllers.deleteCharacter)
+router.delete('/:id/content/content_id', characterControllers.deleteContent)
 
 module.exports = router

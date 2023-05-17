1bb937d3ebe3dea15abc2be3efc4f3db0a994dc9
JonMenez <joanf.gimenez@gmail.com>
2023-05-16 21:27:15 -0300

    [ADD] DB

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config();
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT } = process.env
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT, DB_RAILWAY } = process.env
 
-const database = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`)
+const database = new Sequelize(DB_RAILWAY)
 
 module.exports = database
